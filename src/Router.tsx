69231ac1dbb6066b587127d1053734d47208e902
PedroMakengo <pedromakengomiguel@gmail.com>
2023-03-17 23:45:02 +0100

    Terminando as telas e montando a lógica

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/Home'
 import { CompleteOrderPage } from './pages/CompleteOrder'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { OrderConfirmedPage } from './pages/OrderConfirmed'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/completeOrder" element={<CompleteOrderPage />} />
+        <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
       </Route>
     </Routes>
   )
